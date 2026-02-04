d7acd2625abd87717b6be1184f94c4c31fd7bef9
Samit Singh <samitsingh8595@gmail.com>
2026-02-04 15:14:15 +0530

    Final Version

--- src/Components/UI/Headers.jsx
+++ src/Components/UI/Headers.jsx
@@ -1,50 +1,63 @@
-import { useState } from "react"
-import { GiHamburgerMenu } from "react-icons/gi"
-import { NavLink } from "react-router-dom"
-export const Headers = () => {
-    const [show, setShow] = useState(false);
-    const handleButtonToggle = () => {
-        return setShow(!show)
-    }
-    return(
-        <header>
-            <div className="container">
-                <div className="grid navbar-grid">
-                    <div className="Logo">
-                        <NavLink to="/" style={{textDecoration:"none"}}>
-                        <h1>WorldAtlas</h1>
-                        </NavLink>
-                    </div>
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { NavLink } from "react-router-dom";
 
-                    <nav className={show? "menu-mobile":"menu-web"}>
-                        <ul>
-                            <li>
-                                <NavLink to={"/"} style={{textDecoration:"none"}}>
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to={"/about"} style={{textDecoration:"none"}}>
-                                    About
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to={"/country"} style={{textDecoration:"none"}}>
-                                    Country
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink to={"/contact"} style={{textDecoration:"none"}}>
-                                    Contact
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </nav>
-                    <div className="ham-menu">
-                        <button onClick={handleButtonToggle}><GiHamburgerMenu/></button>
-                    </div>
-                </div>
-            </div>
-        </header>
-    )
-}
+export const Headers = () => {
+  const [show, setShow] = useState(false);
+
+  const handleButtonToggle = () => {
+    setShow(!show);
+  };
+
+  const closeMenu = () => {
+    setShow(false);
+  };
+
+  return (
+    <header>
+      <div className="container">
+        <div className="grid navbar-grid">
+          <div className="Logo">
+            <NavLink to="/" style={{ textDecoration: "none" }}>
+              <h1>WorldAtlas</h1>
+            </NavLink>
+          </div>
+
+          <nav className={show ? "menu-mobile" : "menu-web"}>
+            <ul className="menu-mobile">
+              <li onClick={closeMenu}>
+                <NavLink to="/" style={{ textDecoration: "none" }}>
+                  Home
+                </NavLink>
+              </li>
+
+              <li onClick={closeMenu}>
+                <NavLink to="/about" style={{ textDecoration: "none" }}>
+                  About
+                </NavLink>
+              </li>
+
+              <li onClick={closeMenu}>
+                <NavLink to="/country" style={{ textDecoration: "none" }}>
+                  Country
+                </NavLink>
+              </li>
+
+              <li onClick={closeMenu}>
+                <NavLink to="/contact" style={{ textDecoration: "none" }}>
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <div className="ham-menu">
+            <button onClick={handleButtonToggle}>
+              <GiHamburgerMenu />
+            </button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
